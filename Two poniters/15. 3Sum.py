c6142253cba3022c184638a296dc6d0fbd4863db
Krishna Lamsal <zerokl7600@gmail.com>
2025-01-08 22:43:33 +0545

    3sum

--- Two poniters/15. 3Sum.py
+++ Two poniters/15. 3Sum.py
@@ -1,35 +1,31 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        print(nums)
-        i = 0
-        l = i + 1
-        r = len(nums) - 1
-
         answer = []
 
-        for i in range(len(nums) - 1):
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
             target = -nums[i]
-            l = 0
-            r = len(nums) - 1
+            l, r = i + 1, len(nums) - 1
+
             while l < r:
-                if l == i:
-                    l += 1
-                    continue
-                if r == i:
-                    r -= 1
-                    continue
                 value = nums[l] + nums[r]
-                print((l, r))
-                print(value)
+
                 if value == target:
-                    ans = sorted([nums[i], nums[l], nums[r]])
-                    if ans not in answer:
-                        answer.append(ans)
-                    break
-                elif value < 0:
+                    answer.append([nums[i], nums[l], nums[r]])
                     l += 1
-                else:
                     r -= 1
+
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r + 1]:
+                        r -= 1
+
+                elif value > target:
+                    r -= 1
+                else:
+                    l += 1
 
         return answer
