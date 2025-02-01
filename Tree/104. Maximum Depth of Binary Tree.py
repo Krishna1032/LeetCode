256a78dcc03610156a5caebf1f9a70407160bc05
Krishna Lamsal <zerokl7600@gmail.com>
2025-02-01 20:38:31 +0545

    better

--- Tree/104. Maximum Depth of Binary Tree.py
+++ Tree/104. Maximum Depth of Binary Tree.py
@@ -6,31 +6,10 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        def get_depth(root, node, depth=1):
-            if root is None:
-                return -1
-            if node == root:
-                return depth
-
-            left = get_depth(root.left, node, depth + 1)
-            if left != -1:
-                return left
-
-            return get_depth(root.right, node, depth + 1)
-
         if not root:
             return 0
-        stack = []
-        stack.append(root)
-        max_depth = 1
-        while stack:
-            node = stack.pop()
-            if node.right:
-                stack.append(node.right)
-            if node.left:
-                stack.append(node.left)
-            if not node.right and not node.left:
-                depth = get_depth(root, node)
-                # print(node.val, depth)
-                max_depth = max(max_depth, depth)
-        return max_depth
+
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+
+        return 1 + max(left, right)
